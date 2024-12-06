27c0789f5bdeff0656896d0c52567ba4ab0f4e71
ibrahim <ibrahimmohamed200811@gmail.com>
2024-12-06 15:27:12 +0100

    ik heb iets geschrijven

--- java.js
+++ java.js
@@ -19,3 +19,17 @@ function cookiemelding(){
     document.querySelector(".cookie-message").style.display="none"
     console.log("decline");
 }
+{
+ // Zet de begroeting in het div-element met id "greeting"
+ document.getElementById("greeting").textContent = greeting;
+}
+ 
+// Roep de functie aan zodra de pagina is geladen
+window.onload = getTimeBasedGreeting;
+ 
+ 
+function toggleMenu() {
+    //Elke keer dat deze functie wordt gecalled voeg de show-mobile-nav toe aan de #mobile-nav.
+    //Als de #mobile-nav al actief is omdat er al 1 keer is op geklikt, haal de class weer weg
+    document.getElementById("mobile-nav").classList.toggle("show-mobile-nav");
+  }
